9760bfc18c1f61fb3bb433f5511583c305d2089a
Antoniobellom <61624925+Antoniobellom@users.noreply.github.com>
2021-11-30 12:57:27 -0300

    paginaLogin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './Componentes/Login';
+import UserLogIn from './Componentes/UserLogIn';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer  from './Componentes/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Login />
+    <div className="container">
+    <UserLogIn />
     </div>
+    <Footer />
+    </>
   );
 }
 
